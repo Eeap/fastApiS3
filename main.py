9269fc2446c843556633971d19c823f8aec9893c
Eeap <kdg97811@naver.com>
2022-11-29 01:39:37 +0900

    feat: add upload image service & api

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI
-from s3Service import s3Service
+from fastapi import FastAPI, UploadFile, File
+from s3Service import s3service
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 @app.get("/{name}")
 async def bucket(name: str):
-    s3 = s3Service()
+    s3 = s3service()
     res = s3.makeBucket(name)
+    return res
+@app.post("/upload")
+async def put_images(file: UploadFile = File(None)):
+    s3 = s3service()
+    res = s3.upload_file(file)
     return res
--- s3Service.py
+++ s3Service.py
@@ -5,17 +5,17 @@ import boto3
 import pandas as pd
 from botocore.exceptions import ClientError
 
-
-class s3Service:
+class s3service:
     def __init__(self):
         data = pd.read_csv('s3key.csv')
         self.access_key = data['Access key ID'].to_list()[0]
         self.secret_key = data['Secret access key'].to_list()[0]
         self.client = boto3.client(
             's3',
-            aws_access_key_id = self.access_key,
-            aws_secret_access_key= self.secret_key
+            aws_access_key_id=self.access_key,
+            aws_secret_access_key=self.secret_key
         )
+        self.bucket_name = ""
     def makeBucket(self,name):
         policy = {
             "Version": "2012-10-17",
@@ -42,8 +42,19 @@ class s3Service:
                 CreateBucketConfiguration={'LocationConstraint': 'ap-northeast-2'}
             )
             self.client.put_bucket_policy(Bucket=name,Policy=json.dumps(policy))
+            self.bucket_name = name
         except ClientError:
             logging.exception("clientError")
             raise
 
+        return res
+
+    def upload_file(self, file):
+        # filename 테스트 후 이름 바꾸기
+        try:
+            res = self.client.upload_fileobj(file.file, self.bucket_name, file.filename,
+                                             ExtraArgs={'ContentType': file.content_type, 'ACL': 'public-read'})
+        except ClientError as e:
+            logging.error(e)
+            raise
         return res
